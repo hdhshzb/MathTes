4113bfde01a82a53d0728d44b4ba5dd0961f6959
hdhshzb <80612297+hdhshzb@users.noreply.github.com>
2021-07-09 17:16:32 +0600

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,33 @@
+let but = document.getElementById('finish');
+but.addEventListener('click' , checkTest);
+
+function checkTest() {
+    let result = 0;
+
+    let a1 = document.getElementById('q1').value;
+    if(a1 === '4'){
+        result++
+    }
+
+    let a2 = document.getElementById('q2').value;
+    if(a2 === '25'){
+        result++
+    }
+
+    let a3 = document.getElementById('q3').value;
+    if(a3 === '4'){
+        result++
+    }
+
+    let a4 = document.getElementById('q4').value;
+    if(a4 === '5'){
+        result++
+    }
+
+    let a5 = document.getElementById('q5').value;
+    if(a5 === '40'){
+        result++
+    }
+
+    alert(`Правильных ответов ${result}`)
+}
